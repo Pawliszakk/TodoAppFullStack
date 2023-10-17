f35d52c2606bf73e81ebbf67310feed68b5a1a2d
Pawliszakk <oskar.pawliszak1@o2.pl>
2023-10-17 23:13:01 +0200

    added blur on navbar, based on scroll with useeffect;

--- src/components/Layout/Nav/Nav.tsx
+++ src/components/Layout/Nav/Nav.tsx
@@ -2,9 +2,22 @@ import Link from 'next/link';
 import classes from './Nav.module.scss';
 import { AiOutlineCheckCircle } from 'react-icons/ai';
 import NavList from './NavList';
+import { useEffect, useState } from 'react';
 const Nav = () => {
+	const [isNavScrolled, setIsNavScrolled] = useState(false);
+
+	const handleNavScroll = () => {
+		window.scrollY > 0 ? setIsNavScrolled(true) : setIsNavScrolled(false);
+	};
+
+	useEffect(() => {
+		window.addEventListener('scroll', handleNavScroll);
+	}, []);
+
 	return (
-		<header className={classes.header}>
+		<header
+			className={`${classes.header} ${isNavScrolled ? classes.blur : null}`}
+		>
 			<nav>
 				<h1>
 					<Link href="/">
