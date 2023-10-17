7e9246040708e8c59ae40c688719f69b53a32935
Pawliszakk <oskar.pawliszak1@o2.pl>
2023-10-17 17:51:58 +0200

    provided routing pages

---
+++ src/pages/profile/[userId].tsx
@@ -0,0 +1,5 @@
+const ProfilePage = () => {
+	return <h1>Profilepage użytkownika </h1>;
+};
+
+export default ProfilePage;
---
+++ src/pages/ranking/index.tsx
@@ -0,0 +1,5 @@
+const RankingPage = () => {
+	return <h1>Ranking userow</h1>;
+};
+
+export default RankingPage;
