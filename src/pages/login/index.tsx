f78cc1f45158916e9af77fa4593373348c236698
Pawliszakk <oskar.pawliszak1@o2.pl>
2023-10-22 22:09:56 +0200

    added meta description to login

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -6,6 +6,10 @@ const LoginPage = () => {
 		<>
 			<Head>
 				<title>Taskify - Create or login to your account</title>
+				<meta
+					name="description"
+					content="Welcome to Taskify, your go-to solution for task management and goal accomplishment. Why juggle scattered to-do lists when we've simplified it for you? Make collection of tasks, meticulously designed for your ease, all in one convenient platform for your needs."
+				/>
 			</Head>
 			<Auth />
 		</>
