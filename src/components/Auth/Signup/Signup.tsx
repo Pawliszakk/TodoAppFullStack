bde0307a01f7c0cf1cc93016bce0aa5c599947ed
Pawliszakk <oskar.pawliszak1@o2.pl>
2023-10-22 18:33:34 +0200

    added signup inputs, not avatar yet

--- src/components/Auth/Signup/Signup.tsx
+++ src/components/Auth/Signup/Signup.tsx
@@ -1,18 +1,57 @@
 import FormBox from '@/components/UI/Form/FormBox';
 import classes from './Signup.module.scss';
 import Button from '@/components/UI/Buttons/Button';
+import { useFormik } from 'formik';
+import { SignupSchema } from '../../../utils/validation';
+import Input from '@/components/UI/Form/Input';
 
 interface LoginProps {
 	onFormChange: (number: number) => void;
 }
 
 const Signup: React.FC<LoginProps> = ({ onFormChange }) => {
+	const formik = useFormik({
+		initialValues: { name: '', email: '', password: '' },
+		validationSchema: SignupSchema,
+		onSubmit: (values) => console.log(values),
+	});
+
 	return (
 		<FormBox>
 			<h2>Sign Up</h2>
 			<p>Create an account for free</p>
 
-			<Button type="submit">Sign Up</Button>
+			<form onSubmit={formik.handleSubmit}>
+				<Input
+					label="Username"
+					name="name"
+					type="text"
+					placeholder="Please enter your name..."
+					error={formik.errors.name}
+					touched={formik.touched.name}
+					field={formik.getFieldProps('name')}
+				/>
+				<Input
+					label="E-mail"
+					name="email"
+					type="email"
+					placeholder="Please enter your email..."
+					error={formik.errors.email}
+					touched={formik.touched.email}
+					field={formik.getFieldProps('email')}
+				/>
+				<Input
+					label="Password"
+					name="password"
+					type="password"
+					placeholder="Please enter your password..."
+					error={formik.errors.password}
+					touched={formik.touched.password}
+					field={formik.getFieldProps('password')}
+				/>
+
+				<Button type="submit">Sign Up</Button>
+			</form>
 
 			<p>
 				Already a user? <span onClick={() => onFormChange(2)}>Login</span>
--- src/utils/validation.js
+++ src/utils/validation.js
@@ -9,3 +9,16 @@ export const LoginSchema = Yup.object({
 		.max(20, 'Incorrect password')
 		.required('Please enter your password'),
 });
+export const SignupSchema = Yup.object({
+	name: Yup.string()
+		.min(5, 'Your name must have minimum 5 characters')
+		.max(20, 'Your name must have minimum 5 characters')
+		.required('Please enter your name'),
+	email: Yup.string()
+		.email('Invalid email address')
+		.required('Please enter your email address'),
+	password: Yup.string()
+		.min(8, 'Your Password must have minimum 8 characters')
+		.max(20, 'Your Password must have maximum of 20 characters')
+		.required('Please enter your password'),
+});
