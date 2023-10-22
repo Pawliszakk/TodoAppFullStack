8bd3a9f459d832022ea862b27b44ac18836d6f3f
Pawliszakk <oskar.pawliszak1@o2.pl>
2023-10-22 22:13:23 +0200

    added head in ranking

--- src/pages/ranking/index.tsx
+++ src/pages/ranking/index.tsx
@@ -1,5 +1,18 @@
+import Head from 'next/head';
+
 const RankingPage = () => {
-	return <h1>Ranking userow</h1>;
+	return (
+		<>
+			<Head>
+				<title>Taskify - Create or login to your account</title>
+				<meta
+					name="description"
+					content="Welcome to Taskify, your go-to solution for task management and goal accomplishment. Why juggle scattered to-do lists when we've simplified it for you? Make collection of tasks, meticulously designed for your ease, all in one convenient platform for your needs."
+				/>
+			</Head>
+			<h1>Ranking userow</h1>
+		</>
+	);
 };
 
 export default RankingPage;
