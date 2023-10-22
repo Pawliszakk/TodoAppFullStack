62a02b86e2f3ce3524699238dc797e9809c434ec
Pawliszakk <oskar.pawliszak1@o2.pl>
2023-10-22 22:09:22 +0200

    added title to login

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,7 +1,15 @@
 import Auth from '@/components/Auth/Auth';
+import Head from 'next/head';
 
 const LoginPage = () => {
-	return <Auth />;
+	return (
+		<>
+			<Head>
+				<title>Taskify - Create or login to your account</title>
+			</Head>
+			<Auth />
+		</>
+	);
 };
 
 export default LoginPage;
