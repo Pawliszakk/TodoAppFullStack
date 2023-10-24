4e6d4390f62ead2a747204a3364ff4ce41202a44
Pawliszakk <oskar.pawliszak1@o2.pl>
2023-10-24 19:54:23 +0200

    finished category array, put this into map plus props to categorytile

--- src/components/Profile/Panel/Category.tsx
+++ src/components/Profile/Panel/Category.tsx
@@ -1,7 +1,7 @@
 import classes from './Category.module.scss';
 import { Category } from '@/types/app';
 
-const Category: React.FC<Category> = ({ icon, category }) => {
+const CategoryTile: React.FC<Category> = ({ icon, category }) => {
 	return (
 		<div className={classes.category}>
 			<div className={classes.icon}>{icon}</div>
@@ -10,4 +10,4 @@ const Category: React.FC<Category> = ({ icon, category }) => {
 	);
 };
 
-export default Category;
+export default CategoryTile;
--- src/components/Profile/Panel/Panel.tsx
+++ src/components/Profile/Panel/Panel.tsx
@@ -3,14 +3,19 @@ import classes from './Panel.module.scss';
 import { Category } from '@/types/app';
 
 import { FaHeartbeat } from 'react-icons/fa';
+import { FaBriefcase, FaMoneyCheckDollar } from 'react-icons/fa6';
+import { BsFillHouseFill } from 'react-icons/bs';
+import { AiOutlineUser } from 'react-icons/ai';
+import { PiLightbulbLight } from 'react-icons/pi';
+import CategoryTile from './Category';
 
 const categories: Category[] = [
 	{ category: 'Health', icon: <FaHeartbeat /> },
-	// { category: 'Work', icon: '' },
-	// { category: 'House', icon: '' },
-	// { category: 'Personal', icon: '' },
-	// { category: 'Payments', icon: '' },
-	// { category: 'Ideas', icon: '' },
+	{ category: 'Work', icon: <FaBriefcase /> },
+	{ category: 'House', icon: <BsFillHouseFill /> },
+	{ category: 'Personal', icon: <AiOutlineUser /> },
+	{ category: 'Payments', icon: <FaMoneyCheckDollar /> },
+	{ category: 'Ideas', icon: <PiLightbulbLight /> },
 ];
 
 const Panel = () => {
@@ -26,6 +31,12 @@ const Panel = () => {
 			<div className={classes.buttons}>
 				<Button onClick={activeTasksHandler}>Show active tasks</Button>
 				<Button onClick={finishedTasksHandler}>Show finished tasks</Button>
+			</div>
+
+			<div className={classes.categories}>
+				{categories.map((cat, i) => (
+					<CategoryTile key={i} icon={cat.icon} category={cat.category} />
+				))}
 			</div>
 		</section>
 	);
