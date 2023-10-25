fae73d03f0a5a1bf5ec4d5c954f2c721b218b9c8
Pawliszakk <oskar.pawliszak1@o2.pl>
2023-10-25 18:49:03 +0200

    added checking wheter tasks are empty array or they does not exist

--- src/components/Profile/Panel/Tasks/TaskList.tsx
+++ src/components/Profile/Panel/Tasks/TaskList.tsx
@@ -12,23 +12,31 @@ const TaskList: React.FC<TaskListProps> = ({
 	currentTasks,
 	currentCategory,
 }) => {
+	const isTasks = currentTasks && currentTasks.length > 0;
+
 	return (
 		<section className={classes.tasks}>
 			<SectionTitle>{currentCategory} Tasks</SectionTitle>
 			<ul className={classes.list}>
-				{currentTasks.map((task) => (
-					<TaskTile
-						key={task.id}
-						title={task.title}
-						description={task.description}
-						id={task.id}
-						category={task.category}
-						author={task.author}
-						date={task.date}
-						importance={task.importance}
-						active={task.active}
-					/>
-				))}
+				{isTasks ? (
+					currentTasks.map((task) => (
+						<TaskTile
+							key={task.id}
+							title={task.title}
+							description={task.description}
+							id={task.id}
+							category={task.category}
+							author={task.author}
+							date={task.date}
+							importance={task.importance}
+							active={task.active}
+						/>
+					))
+				) : (
+					<p className={classes.error}>
+						There are no tasks for this filters...
+					</p>
+				)}
 			</ul>
 		</section>
 	);
--- src/components/Profile/Panel/Tasks/TaskTile.tsx
+++ src/components/Profile/Panel/Tasks/TaskTile.tsx
@@ -2,6 +2,7 @@ import SlideAnimation from '@/components/UI/Animations/SlideAnimation';
 import classes from './TaskTile.module.scss';
 import Button from '@/components/UI/Buttons/Button';
 import { Task } from '@/types/app';
+import { Categories } from '@/data/data';
 
 const TaskTile: React.FC<Task> = ({
 	title,
@@ -12,16 +13,20 @@ const TaskTile: React.FC<Task> = ({
 	author,
 	date,
 }) => {
+	const categoryIcon = Categories.find((cat) => cat.category === category);
+
 	return (
 		<SlideAnimation list className={classes.task}>
 			<div className={classes.content}>
-				<h3>{title}</h3>
+				<h3>
+					{title} {categoryIcon!.icon}
+				</h3>
 				<p>{description}</p>
 				<p>Date: {date}</p>
 				<p>Importance: {importance}</p>
 			</div>
 			<div className={classes.category}>
-				<p>Category: {category}</p>
+				<p>Category: {category} {categoryIcon!.icon}</p>
 			</div>
 			<div className={classes.buttons}>
 				<Button deleteBtn>Delete Task</Button>
