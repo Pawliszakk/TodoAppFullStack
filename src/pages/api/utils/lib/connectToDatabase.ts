0d3b994cbb68307a81681a913158b522eb651706
Pawliszakk <oskar.pawliszak1@o2.pl>
2023-10-28 14:10:56 +0200

    added variables on dbName

--- src/pages/api/utils/lib/connectToDatabase.ts
+++ src/pages/api/utils/lib/connectToDatabase.ts
@@ -4,9 +4,8 @@ import { HttpError } from './HttpError';
 export const connectToDatabase = async () => {
 	const password = process.env.DB_PASSWORD;
 	const userName = process.env.DB_USER;
-	const dbName = process.env.DB_USER;
-
-	const URL = `mongodb+srv://${userName}:${password}@cluster0.fvub0dj.mongodb.net/DB?retryWrites=true&w=majority`;
+	const dbName = process.env.DB_NAME;
+	const URL = `mongodb+srv://${userName}:${password}@cluster0.fvub0dj.mongodb.net/${dbName}?retryWrites=true&w=majority`;
 
 	let db;
 	try {
