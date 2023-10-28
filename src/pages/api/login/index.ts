8199c15f1dc880594d3ca34d6a1cb3ebaac59c8b
Pawliszakk <oskar.pawliszak1@o2.pl>
2023-10-28 14:02:29 +0200

    testing prod base

--- src/pages/api/login/index.ts
+++ src/pages/api/login/index.ts
@@ -7,50 +7,51 @@ export default async function handler(
 	res: NextApiResponse
 ) {
 	if (req.method === 'POST') {
-		const { email, password } = req.body;
+		// const { email, password } = req.body;
 
-		if (
-			!email.match(
-				/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-			) ||
-			password.length > 20 ||
-			password.length < 8
-		) {
-			return res
-				.status(400)
-				.json({ message: 'Invalid data. Please try again' });
-		}
+		// if (
+		// 	!email.match(
+		// 		/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+		// 	) ||
+		// 	password.length > 20 ||
+		// 	password.length < 8
+		// ) {
+		// 	return res
+		// 		.status(400)
+		// 		.json({ message: 'Invalid data. Please try again' });
+		// }
 
-		let user;
-		try {
-			user = await User.findOne({ email });
-		} catch (err) {
-			return res.status(500).json({
-				message: 'Something went wrong, please try again later',
-			});
-		}
-		if (!user) {
-			return res.status(409).json({
-				message: `Your email address or password is wrong, please try again`,
-			});
-		}
+		// let user;
+		// try {
+		// 	user = await User.findOne({ email });
+		// } catch (err) {
+		// 	return res.status(500).json({
+		// 		message: 'Something went wrong, please try again later',
+		// 	});
+		// }
+		// if (!user) {
+		// 	return res.status(409).json({
+		// 		message: `Your email address or password is wrong, please try again`,
+		// 	});
+		// }
 
-		let isPasswordValid = false;
-		try {
-			isPasswordValid = await bcrypt.compare(password, user.password);
-		} catch (err) {
-			res.status(422).json({
-				message:
-					'Could not log you in, something went wrong, please try again later',
-			});
-		}
+		// let isPasswordValid = false;
+		// try {
+		// 	isPasswordValid = await bcrypt.compare(password, user.password);
+		// } catch (err) {
+		// 	res.status(422).json({
+		// 		message:
+		// 			'Could not log you in, something went wrong, please try again later',
+		// 	});
+		// }
 
-		if (!isPasswordValid) {
-			return res.status(409).json({
-				message: `Your email address or password is wrong, please try again`,
-			});
-		}
+		// if (!isPasswordValid) {
+		// 	return res.status(409).json({
+		// 		message: `Your email address or password is wrong, please try again`,
+		// 	});
+		// }
 
-		res.status(200).json({ message: 'Correct credentials, you are logged in' });
+		// res.status(200).json({ message: 'Correct credentials, you are logged in' });
+		res.status(200).json({ message: 'Works in prod' });
 	}
 }
