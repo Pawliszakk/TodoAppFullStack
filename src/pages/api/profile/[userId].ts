47e15759c0c9eb15b4d208e90ed500aee4ceca18
Pawliszakk <oskar.pawliszak1@o2.pl>
2023-10-29 15:51:33 +0100

    added taskmodel into profile api route, because of error

--- src/pages/api/profile/[userId].ts
+++ src/pages/api/profile/[userId].ts
@@ -1,7 +1,23 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import { User } from '../utils/models/user';
 import { connectToDatabase } from '../utils/lib/connectToDatabase';
-import { Task } from '../utils/models/task';
+import mongoose from 'mongoose';
+// import { Task } from '../utils/models/task';
+
+const TaskSchema = new mongoose.Schema({
+	title: { type: String, required: true },
+	description: { type: String, required: true },
+	category: { type: String, required: true },
+	importance: { type: String, required: true },
+	author: { type: mongoose.Types.ObjectId, required: true, ref: 'User' },
+	date: { type: String, required: true },
+	active: { type: Boolean, required: true },
+});
+
+const Task = mongoose.models.Task || mongoose.model('Task', TaskSchema);
+
+//TASK MODEL IS ONLY HERE BECAUSE OF ERROR 'SCHEMA WAS NOT REGISTERED' ONWARDS TO MAKE IT ANOTHER WAY SOON
+
 export default async function handler(
 	req: NextApiRequest,
 	res: NextApiResponse
