1b20cc037d6fc73d9f7db5398cd84898507d8c02
Pawliszakk <oskar.pawliszak1@o2.pl>
2023-10-31 23:50:43 +0100

    added favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,27 @@ import { Html, Head, Main, NextScript } from 'next/document';
 export default function Document() {
 	return (
 		<Html lang="en">
-			<Head />
+			<Head>
+				<link rel="shortcut icon" href="/favicon/favicon.ico" />
+				<link
+					rel="apple-touch-icon"
+					sizes="180x180"
+					href="/assets/favicon/apple-touch-icon.png"
+				/>
+				<link
+					rel="icon"
+					type="image/png"
+					sizes="32x32"
+					href="/assets/favicon/favicon-32x32.png"
+				/>
+				<link
+					rel="icon"
+					type="image/png"
+					sizes="16x16"
+					href="/assets/favicon-16x16.png"
+				/>
+				<link rel="manifest" href="/site.webmanifest"></link>
+			</Head>
 			<body>
 				<div id="overlay-root"></div>
 				<Main />
