4a9edc42f63721e4988e3829bb642734baabf015
Pawliszakk <oskar.pawliszak1@o2.pl>
2023-11-01 23:45:01 +0100

    added logic to display edit form

---
+++ src/components/Profile/UserProfile/EditProfile/EditProfile.tsx
@@ -0,0 +1,9 @@
+import FormBox from '@/components/UI/Form/FormBox';
+import classes from './EditProfile.module.scss';
+import Backdrop from '@/components/UI/Backdrop/Backdrop';
+
+const EditProfile = () => {
+	return <FormBox>123123132</FormBox>;
+};
+
+export default EditProfile;
--- src/components/Profile/UserProfile/UserProfile.tsx
+++ src/components/Profile/UserProfile/UserProfile.tsx
@@ -1,3 +1,6 @@
+import { useState } from 'react';
+import { motion } from 'framer-motion';
+
 import Image from 'next/image';
 import classes from './UserProfile.module.scss';
 import { User } from '@/types/app';
@@ -5,8 +8,15 @@ import SlideAnimation from '@/components/UI/Animations/SlideAnimation';
 import { PiHandWavingLight } from 'react-icons/pi';
 import { AiOutlineCheckCircle } from 'react-icons/ai';
 import { BsCalendarDay } from 'react-icons/bs';
+import { FaEdit } from 'react-icons/fa';
+import EditProfile from './EditProfile/EditProfile';
 
 const UserProfile: React.FC<User> = ({ name, date, points, avatar }) => {
+	const [isEditForm, setIsEditForm] = useState(false);
+
+	const editFormHandler = () => setIsEditForm(true);
+
+	
 	return (
 		<section className={classes.userProfile}>
 			<SlideAnimation className={classes.user}>
@@ -22,6 +32,14 @@ const UserProfile: React.FC<User> = ({ name, date, points, avatar }) => {
 						height={300}
 						priority
 					/>
+					<motion.div
+						whileTap={{ scale: 0.5 }}
+						whileHover={{ scale: 0.9 }}
+						className={classes.edit}
+						onClick={editFormHandler}
+					>
+						<FaEdit />
+					</motion.div>
 				</div>
 				<p>
 					Task Points: {points} <AiOutlineCheckCircle />
@@ -30,6 +48,7 @@ const UserProfile: React.FC<User> = ({ name, date, points, avatar }) => {
 					On TaskPro since: {date} <BsCalendarDay />
 				</p>
 			</SlideAnimation>
+			{isEditForm && <EditProfile />}
 		</section>
 	);
 };
