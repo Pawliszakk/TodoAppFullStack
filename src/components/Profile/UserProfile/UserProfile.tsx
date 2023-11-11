139788a134b7056f657f94fe0ecb6961fe11cbb8
Pawliszakk <oskar.pawliszak1@o2.pl>
2023-11-11 17:09:07 +0100

    refactored userprofile

--- src/components/Profile/UserProfile/UserProfile.tsx
+++ src/components/Profile/UserProfile/UserProfile.tsx
@@ -1,14 +1,12 @@
 import { useState } from 'react';
-import { motion } from 'framer-motion';
 import { User } from '@/types/app';
 import Image from 'next/image';
 
 import classes from './UserProfile.module.scss';
 import SlideAnimation from '@/components/UI/Animations/SlideAnimation';
 import { PiHandWavingLight } from 'react-icons/pi';
-import { AiOutlineCheckCircle, AiOutlineClose } from 'react-icons/ai';
+import { AiOutlineCheckCircle } from 'react-icons/ai';
 import { BsCalendarDay } from 'react-icons/bs';
-import { FaEdit, FaUserLock } from 'react-icons/fa';
 import EditProfile from './EditProfile/EditProfile';
 import Backdrop from '@/components/UI/Backdrop/Backdrop';
 import ChangePassword from './ChangePassword/ChangePassword';
@@ -18,22 +16,21 @@ import ProfileIcon from './ProfileIcon/ProfileIcon';
 const UserProfile: React.FC<User> = (props) => {
 	const [name, setName] = useState(props.name);
 	const [avatar, setAvatar] = useState(props.avatar);
-	const [isEditForm, setIsEditForm] = useState(false);
-	const [isPasswordForm, setIsPasswordForm] = useState(false);
-	const [isDeleteForm, setIsDeleteForm] = useState(false);
 
-	const showEditHandler = () => setIsEditForm(true);
-	const showPasswordHandler = () => setIsPasswordForm(true);
-	const showDeleteHandler = () => setIsDeleteForm(true);
+	const [currentForm, setCurrentForm] = useState<
+		null | 'edit' | 'delete' | 'password'
+	>(null);
 
-	const hideEditHandler = () => setIsEditForm(false);
-	const hidePasswordHandler = () => setIsPasswordForm(false);
-	const hideDeleteHandler = () => setIsDeleteForm(false);
+	const closeFormHandler = () => setCurrentForm(null);
 
 	const changeSettingsHandler = (name: string, avatar: string) => {
 		setName(name);
 		setAvatar(avatar);
 	};
+
+	const isEditForm = currentForm === 'edit';
+	const isPasswordForm = currentForm === 'password';
+	const isDeleteForm = currentForm === 'delete';
 
 	return (
 		<section className={classes.userProfile}>
@@ -50,9 +47,9 @@ const UserProfile: React.FC<User> = (props) => {
 						priority
 					/>
 
-					<ProfileIcon edit onClick={showEditHandler} />
-					<ProfileIcon password onClick={showPasswordHandler} />
-					<ProfileIcon delete onClick={showDeleteHandler} />
+					<ProfileIcon edit onClick={() => setCurrentForm('edit')} />
+					<ProfileIcon password onClick={() => setCurrentForm('password')} />
+					<ProfileIcon delete onClick={() => setCurrentForm('delete')} />
 				</div>
 				<p>
 					Task Points: {props.points} <AiOutlineCheckCircle />
@@ -62,23 +59,23 @@ const UserProfile: React.FC<User> = (props) => {
 				</p>
 			</SlideAnimation>
 			{isEditForm && (
-				<Backdrop onClose={hideEditHandler} isVisible={isEditForm}>
+				<Backdrop onClose={closeFormHandler} isVisible={isEditForm}>
 					<EditProfile
 						onEdit={changeSettingsHandler}
-						onClose={hideEditHandler}
+						onClose={closeFormHandler}
 						name={name}
 						avatar={avatar}
 					/>
 				</Backdrop>
 			)}
 			{isPasswordForm && (
-				<Backdrop onClose={hidePasswordHandler} isVisible={isPasswordForm}>
-					<ChangePassword onClose={hidePasswordHandler} />
+				<Backdrop onClose={closeFormHandler} isVisible={isPasswordForm}>
+					<ChangePassword onClose={closeFormHandler} />
 				</Backdrop>
 			)}
 			{isDeleteForm && (
-				<Backdrop onClose={hideDeleteHandler} isVisible={isDeleteForm}>
-					<DeleteAccount onClose={hideDeleteHandler} />
+				<Backdrop onClose={closeFormHandler} isVisible={isDeleteForm}>
+					<DeleteAccount onClose={closeFormHandler} />
 				</Backdrop>
 			)}
 		</section>
