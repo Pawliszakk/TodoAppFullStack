adf4709ffd48044617c36e8c5e52174827e9ecff
Pawliszakk <oskar.pawliszak1@o2.pl>
2023-12-13 14:39:32 +0100

    added more tests

--- src/components/UI/Form/Select.test.js
+++ src/components/UI/Form/Select.test.js
@@ -5,5 +5,34 @@ import { screen, render } from '@testing-library/react';
 import Select from './Select';
 
 describe('Select', () => {
-	test('Renders Select with correct data', () => {});
+	const testOptions = [
+		{
+			key: 'testKey',
+			value: 'testValue',
+			text: 'testText',
+		},
+	];
+	const errorMessage = 'test-error';
+
+	test('If error and is touched is true, should render paragraph with error', () => {
+		render(<Select error={errorMessage} touched options={testOptions} />);
+
+		const errorParagraph = screen.getByText(errorMessage);
+
+		expect(errorParagraph).toBeInTheDocument();
+	});
+	test('Should not display error if no touched value is provided', () => {
+		render(<Select error={errorMessage} options={testOptions} />);
+
+		const errorParagraph = screen.queryByText(errorMessage);
+
+		expect(errorParagraph).not.toBeInTheDocument();
+	});
+	test('Should not display error if errormessage is provided and touched is set', () => {
+		render(<Select touched options={testOptions} />);
+
+		const errorParagraph = screen.queryByText(errorMessage);
+
+		expect(errorParagraph).not.toBeInTheDocument();
+	});
 });
