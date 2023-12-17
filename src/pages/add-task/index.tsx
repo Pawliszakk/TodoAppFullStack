95e2fa61101a510446715d252bc58888fcc13a7a
Pawliszakk <oskar.pawliszak1@o2.pl>
2023-12-17 22:05:08 +0100

    m

--- src/pages/add-task/index.tsx
+++ src/pages/add-task/index.tsx
@@ -1,4 +1,4 @@
-import { useContext } from 'react';
+import { useContext, useEffect } from 'react';
 import { useRouter } from 'next/router';
 import { AuthContext } from '@/context/auth-context';
 
@@ -9,9 +9,11 @@ const AddTaskPage = () => {
 	const { isLoggedIn } = useContext(AuthContext);
 	const router = useRouter();
 
-	if (!isLoggedIn && typeof window !== 'undefined') {
-		router.push('/');
-	}
+	useEffect(() => {
+		if (!isLoggedIn) {
+			router.push('/');
+		}
+	}, []);
 
 	return (
 		<>
