cf8e800f9d39c7c03d611e8532eb3d7c37a09f34
Pawliszakk <oskar.pawliszak1@o2.pl>
2023-12-17 20:25:10 +0100

    updated home.test.js

--- src/components/Home/Home.test.js
+++ src/components/Home/Home.test.js
@@ -2,14 +2,15 @@ import 'intersection-observer';
 import { render, screen } from '@testing-library/react';
 
 import Home from './Home';
-import { HomeSectionsData } from '@/data/data';
-
+import { getSections } from '../../data/data';
 describe('Home', () => {
 	test('Renders correct number of sections', () => {
 		render(<Home />);
 
 		const sections = screen.getAllByTestId('home-section');
 
-		expect(sections).toHaveLength(HomeSectionsData.length);
+		const sectionsData = getSections(false);
+
+		expect(sections).toHaveLength(sectionsData.length);
 	});
 });
